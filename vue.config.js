3f3659d7f3397bf54ad7317084ea9045b35aed83
renqiang <renqiang@lswooks.com>
2023-01-17 18:01:00 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+    },
+  },
+  configureWebpack: {
+    resolve: { extensions: ['.ts', '.tsx', '.js', '.json'] },
+    module: {
+      rules: [
+        // 从这里复制下面的代码就可以了
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          enforce: 'pre',
+          loader: 'ts-loader',
+        },
+      ],
+    },
+  },
 })
